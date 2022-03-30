2079b6c8a98368b153feae57121ca33cb0b670c5
Govind Jeevan <govindjeevan7@gmail.com>
2022-03-31 00:25:01 +0530

    Update load_test.py

--- load_test.py
+++ load_test.py
@@ -1,12 +1,10 @@
 import requests
-from requests.structures import CaseInsensitiveDict
 import ast
 import os
 
 IFFTT_KEY = os.environ.get('IFFTT_KEY')
 URL =  os.environ.get('URL')
 HEADERS = os.environ.get('HEADERS')
-print(HEADERS)
 headers = ast.literal_eval(str(HEADERS))
 
 resp = requests.get(URL, headers=headers)
